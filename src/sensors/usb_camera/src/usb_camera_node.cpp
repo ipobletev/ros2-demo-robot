7b077ded4d9d6e1b5b485c7e28994be6305ba2e2
ismael.pbt <ismael.poblete@wisely.cl>
2025-03-21 21:28:50 -0300

    update random image to camera 0

--- src/sensors/usb_camera/src/usb_camera_node.cpp
+++ src/sensors/usb_camera/src/usb_camera_node.cpp
@@ -1,45 +1,35 @@
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
-#include "std_msgs/msg/header.hpp"
-#include <chrono>
+#include <rclcpp/rclcpp.hpp>
 #include <cv_bridge/cv_bridge.hpp>
-#include <image_transport/image_transport.hpp>
+#include <sensor_msgs/msg/image.hpp>
 #include <opencv2/opencv.hpp>
 
-using namespace std::chrono_literals;
-
-class MinimalImagePublisher : public rclcpp::Node {
+class USBCameraNode : public rclcpp::Node {
 public:
-  MinimalImagePublisher() : Node("usb_camera_node"), count_(0) {
-    publisher_ =
-        this->create_publisher<sensor_msgs::msg::Image>("random_image", 10);
-    timer_ = this->create_wall_timer(
-        500ms, std::bind(&MinimalImagePublisher::timer_callback, this));
-  }
+    USBCameraNode() : Node("usb_camera_node") {
+        publisher_ = this->create_publisher<sensor_msgs::msg::Image>("camera/image", 10);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), 
+            std::bind(&USBCameraNode::capture_image, this));
+        cap_.open(0); // Camera 
+    }
 
 private:
-  void timer_callback() {
+    void capture_image() {
+        cv::Mat frame;
+        cap_ >> frame; // Capture an image from the camera
+        if (!frame.empty()) {
+            auto msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame).toImageMsg();
+            publisher_->publish(*msg);
+        }
+    }
 
-    cv::Mat my_image(cv::Size(640, 480), CV_8UC3);
-    cv::randu(my_image, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255));
-    msg_ = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", my_image)
-               .toImageMsg();
-    publisher_->publish(*msg_.get());
-    RCLCPP_INFO(this->get_logger(), "Image %ld published", count_);
-    count_++;
-  }
-  rclcpp::TimerBase::SharedPtr timer_;
-  sensor_msgs::msg::Image::SharedPtr msg_;
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
-  size_t count_;
+    cv::VideoCapture cap_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char *argv[]) {
-  rclcpp::init(argc, argv);
-  // create a ros2 node
-  auto node = std::make_shared<MinimalImagePublisher>();
-  // process ros2 callbacks until receiving a SIGINT (ctrl-c)
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<USBCameraNode>());
+    rclcpp::shutdown();
+    return 0;
 }
