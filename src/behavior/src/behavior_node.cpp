2ec79d09f8be68baf87dac35be0582dcf30e290d
Ismael Poblete <ismael.pbt@gmail.com>
2025-03-23 21:29:42 -0300

    english comments

--- src/behavior/src/behavior_node.cpp
+++ src/behavior/src/behavior_node.cpp
@@ -26,23 +26,23 @@ int main(int argc, char **argv)
 
     BT::BehaviorTreeFactory factory;
 
-    // Registrar las acciones locales
+    // Register local actions
     factory.registerSimpleAction("node_a", [](BT::TreeNode& tree_node) -> BT::NodeStatus {
-        RCLCPP_INFO(rclcpp::get_logger("node_a"), "Ejecutando node_a");
+        RCLCPP_INFO(rclcpp::get_logger("node_a"), "Executing node_a");
         return BT::NodeStatus::SUCCESS;
     });
 
     factory.registerSimpleAction("node_b", [](BT::TreeNode& tree_node) -> BT::NodeStatus {
-        RCLCPP_INFO(rclcpp::get_logger("node_b"), "Ejecutando node_b");
+        RCLCPP_INFO(rclcpp::get_logger("node_b"), "Executing node_b");
         return BT::NodeStatus::SUCCESS;
     });
 
-    // Registrar los subárboles
+    // Register subtrees
     factory.registerBehaviorTreeFromFile("src/behavior/behavior_trees/bt_node_a.xml");
     factory.registerBehaviorTreeFromFile("src/behavior/behavior_trees/bt_node_b.xml");
     factory.registerBehaviorTreeFromFile("src/behavior/bt_main.xml");
     
-    // Crear el árbol principal utilizando su ID, ya que se han registrado los subárboles
+    // Create the main tree using its ID, as the subtrees have been registered
     auto tree = factory.createTree("bt_main");
 
     while (rclcpp::ok() && tree.rootNode()->status() != BT::NodeStatus::SUCCESS)
